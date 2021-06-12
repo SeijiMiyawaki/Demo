c8763a0c3257e5073f6ce430b544859aaef17289
seiji <pillow1207@gmail.com>
2021-06-12 18:20:35 +0900

    @Entity, @Table, @Id, @GeneratedValueの追加。

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,8 +1,23 @@
 package com.example.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
 	private int id;
+	
+	@Column
 	private String name;
+	
 	public int getId() {
 		return id;
 	}
